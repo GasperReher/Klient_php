01b4a528fbb1395756c2295d2b9f4cffd01207f6
GasperReher <gasper.reher@student.um.si>
2019-05-30 10:27:27 +0200

    Prijava

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php
-
 session_start();
  ?>
 <head>
@@ -28,8 +27,6 @@ session_start();
 
 <body>
 <?php
-
-
 $_SERVER['REQUEST_METHOD']
 ?>
 
@@ -37,48 +34,53 @@ $_SERVER['REQUEST_METHOD']
 
 
 <?php
-  if(array_key_exists('isci',$_POST)){
-$fields = array("method" => "mymethod", "email" => "myemail");
-$isci=$_POST["qr"];
-$url = "http://localhost:8880/projekt/rest/upoti/upo/".(int)$isci;
-  $fields = json_encode($fields);
-  $ch = curl_init();
-  curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json; charset=UTF-8"));
-  curl_setopt($ch, CURLOPT_URL, $url);
-  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-  curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  $result["msg"] = curl_exec($ch);
-  $result["http"] = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-  curl_close($ch);
+  if(array_key_exists('prijava',$_POST)){
+    $fields = array("method" => "mymethod", "email" => "myemail");
+    //echo $isci." ".$cat;ž
+    $email=$_POST['email'];
+    $geslo=$_POST['geslo'];
+    $url = "http://localhost:8880/projekt/rest/upoti/upot/";
+      $fields = json_encode($fields);
+      $ch = curl_init();
 
-if (count($result)>0){
-  $streznik = $_SERVER["HTTP_HOST"];
-    // pot do imenika in datoteke, v katerem smo
-    $pot = rtrim(dirname($_SERVER["PHP_SELF"]), "/\\");
-    $streznik = $_SERVER["HTTP_HOST"];
+      curl_setopt($ch, CURLOPT_URL, $url);
+      //curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+      'Content-Type: application/json',
+      'Content-Length: ' . strlen($fields))
+      );
+      $result= curl_exec($ch);
 
-      $pot = rtrim(dirname($_SERVER["PHP_SELF"]), "/\\");
+      curl_close($ch);
+      $obj= json_decode($result,true);
 
-    $id=explode(":", explode(",",$result['msg'])[1])[1];
-      $ime=explode(":", explode(",",$result['msg'])[2])[1];
-        $priimek=explode(":", explode(",",$result['msg'])[4])[1];
-    echo  "ID  ".$id." ime ".$ime." priimek ".$priimek;
-$_SESSION["id"] =$id;
-$_SESSION["ime"] =$ime;
-$_SESSION["priimek"] =$priimek;
-  header("Location: http://$streznik$pot/knjiznica.php");
+$preveri=false;
+            if(isset($obj)){
+            foreach($obj as $i) { //foreach element in $arr
 
+      
+           if($i["email"]==$email &&$i["password"]==$geslo ){
+             $pot = rtrim(dirname($_SERVER["PHP_SELF"]), "/\\");
+             $streznik = $_SERVER["HTTP_HOST"];
+             $preveri=true;
+         $_SESSION["id"] =$i["id"];
+         $_SESSION["ime"] =$i["ime"];
+         $_SESSION["priimek"] =$i["priimek"];;
+           header("Location: http://$streznik$pot/knjiznica.php");
+
+           }
+
+
+          }
+         }
 }
-print_r ( $result);
-
-}
-
  ?>
   <!-- Navigation -->
   <nav class="navbar navbar-light bg-light static-top">
     <div class="container">
-      <a class="navbar-brand" href="#">Knjigomat</a>
+      <a class="navbar-brand" href="index.php">Knjigomat</a>
 
     </div>
   </nav>
@@ -89,188 +91,73 @@ print_r ( $result);
     <div class="container">
       <div class="row">
         <div class="col-xl-9 mx-auto">
-          <h1 class="mb-5">Vnesite vašo QR kodo:</h1>
+          <h1 class="mb-5">Prijava</h1>
         </div>
         <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
-          <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
-            <div class="form-row">
-              <div class="col-12 col-md-9 mb-2 mb-md-0">
-                <input type="text" name="qr" class="form-control form-control-lg" placeholder="Vnesite QR kodo...">
-              </div>
-              <div class="col-12 col-md-3">
-                <button type="submit" name="isci" class="btn btn-block btn-lg btn-primary">Prijava!</button>
-              </div>
-            </div>
-          </form>
+            <form  action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
+  <div class="form-group">
+    <label for="email">Vnesite E-pošto:</label>
+    <input type="email" class="form-control" name="email" id="email">
+  </div>
+  <div class="form-group">
+    <label for="pwd">Vnesite Geslo:</label>
+    <input type="password" class="form-control" name="geslo" id="pwd">
+  </div>
+  <button type="submit" name="prijava" class="btn btn-primary">Prijava</button>
+</form>
         </div>
       </div>
     </div>
   </header>
 
-  <!-- Icons Grid -->
-  <section class="features-icons bg-light text-center">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-4">
-          <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-            <div class="features-icons-icon d-flex">
-              <i class="icon-screen-desktop m-auto text-primary"></i>
-            </div>
-            <h3>Fully Responsive</h3>
-            <p class="lead mb-0">This theme will look great on any device, no matter the size!</p>
-          </div>
-        </div>
-        <div class="col-lg-4">
-          <div class="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
-            <div class="features-icons-icon d-flex">
-              <i class="icon-layers m-auto text-primary"></i>
-            </div>
-            <h3>Bootstrap 4 Ready</h3>
-            <p class="lead mb-0">Featuring the latest build of the new Bootstrap 4 framework!</p>
-          </div>
-        </div>
-        <div class="col-lg-4">
-          <div class="features-icons-item mx-auto mb-0 mb-lg-3">
-            <div class="features-icons-icon d-flex">
-              <i class="icon-check m-auto text-primary"></i>
-            </div>
-            <h3>Easy to Use</h3>
-            <p class="lead mb-0">Ready to use with your own content, or customize the source files!</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
+  <?php
+    if(array_key_exists('prijava',$_POST)){
+      $fields = array("method" => "mymethod", "email" => "myemail");
+      //echo $isci." ".$cat;ž
+      $email=$_POST['email'];
+      $geslo=$_POST['geslo'];
+      $url = "http://localhost:8880/projekt/rest/upoti/upot/";
+        $fields = json_encode($fields);
+        $ch = curl_init();
 
-  <!-- Image Showcases -->
-  <section class="showcase">
-    <div class="container-fluid p-0">
-      <div class="row no-gutters">
+        curl_setopt($ch, CURLOPT_URL, $url);
+        //curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json',
+        'Content-Length: ' . strlen($fields))
+        );
+        $result= curl_exec($ch);
 
-        <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('img/bg-showcase-1.jpg');"></div>
-        <div class="col-lg-6 order-lg-1 my-auto showcase-text">
-          <h2>Fully Responsive Design</h2>
-          <p class="lead mb-0">When you use a theme created by Start Bootstrap, you know that the theme will look great on any device, whether it's a phone, tablet, or desktop the page will behave responsively!</p>
-        </div>
-      </div>
-      <div class="row no-gutters">
-        <div class="col-lg-6 text-white showcase-img" style="background-image: url('img/bg-showcase-2.jpg');"></div>
-        <div class="col-lg-6 my-auto showcase-text">
-          <h2>Updated For Bootstrap 4</h2>
-          <p class="lead mb-0">Newly improved, and full of great utility classes, Bootstrap 4 is leading the way in mobile responsive web development! All of the themes on Start Bootstrap are now using Bootstrap 4!</p>
-        </div>
-      </div>
-      <div class="row no-gutters">
-        <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('img/bg-showcase-3.jpg');"></div>
-        <div class="col-lg-6 order-lg-1 my-auto showcase-text">
-          <h2>Easy to Use &amp; Customize</h2>
-          <p class="lead mb-0">Landing Page is just HTML and CSS with a splash of SCSS for users who demand some deeper customization options. Out of the box, just add your content and images, and your new landing page will be ready to go!</p>
-        </div>
-      </div>
-    </div>
-  </section>
+        curl_close($ch);
+        $obj= json_decode($result,true);
 
-  <!-- Testimonials -->
-  <section class="testimonials text-center bg-light">
-    <div class="container">
-      <h2 class="mb-5">What people are saying...</h2>
-      <div class="row">
-        <div class="col-lg-4">
-          <div class="testimonial-item mx-auto mb-5 mb-lg-0">
-            <img class="img-fluid rounded-circle mb-3" src="img/testimonials-1.jpg" alt="">
-            <h5>Margaret E.</h5>
-            <p class="font-weight-light mb-0">"This is fantastic! Thanks so much guys!"</p>
-          </div>
-        </div>
-        <div class="col-lg-4">
-          <div class="testimonial-item mx-auto mb-5 mb-lg-0">
-            <img class="img-fluid rounded-circle mb-3" src="img/testimonials-2.jpg" alt="">
-            <h5>Fred S.</h5>
-            <p class="font-weight-light mb-0">"Bootstrap is amazing. I've been using it to create lots of super nice landing pages."</p>
-          </div>
-        </div>
-        <div class="col-lg-4">
-          <div class="testimonial-item mx-auto mb-5 mb-lg-0">
-            <img class="img-fluid rounded-circle mb-3" src="img/testimonials-3.jpg" alt="">
-            <h5>Sarah W.</h5>
-            <p class="font-weight-light mb-0">"Thanks so much for making these free resources available to us!"</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
+  $preveri=false;
+              if(isset($obj)){
+              foreach($obj as $i) { //foreach element in $arr
 
-  <!-- Call to Action -->
-  <section class="call-to-action text-white text-center">
-    <div class="overlay"></div>
-    <div class="container">
-      <div class="row">
-        <div class="col-xl-9 mx-auto">
-          <h2 class="mb-4">Ready to get started? Sign up now!</h2>
-        </div>
-        <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
-          <form>
-            <div class="form-row">
-              <div class="col-12 col-md-9 mb-2 mb-md-0">
-                <input type="email" class="form-control form-control-lg" placeholder="Enter your email...">
-              </div>
-              <div class="col-12 col-md-3">
-                <button type="submit" class="btn btn-block btn-lg btn-primary">Sign up!</button>
-              </div>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
-  </section>
 
-  <!-- Footer -->
-  <footer class="footer bg-light">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-6 h-100 text-center text-lg-left my-auto">
-          <ul class="list-inline mb-2">
-            <li class="list-inline-item">
-              <a href="#">About</a>
-            </li>
-            <li class="list-inline-item">&sdot;</li>
-            <li class="list-inline-item">
-              <a href="#">Contact</a>
-            </li>
-            <li class="list-inline-item">&sdot;</li>
-            <li class="list-inline-item">
-              <a href="#">Terms of Use</a>
-            </li>
-            <li class="list-inline-item">&sdot;</li>
-            <li class="list-inline-item">
-              <a href="#">Privacy Policy</a>
-            </li>
-          </ul>
-          <p class="text-muted small mb-4 mb-lg-0">&copy; Your Website 2019. All Rights Reserved.</p>
-        </div>
-        <div class="col-lg-6 h-100 text-center text-lg-right my-auto">
-          <ul class="list-inline mb-0">
-            <li class="list-inline-item mr-3">
-              <a href="#">
-                <i class="fab fa-facebook fa-2x fa-fw"></i>
-              </a>
-            </li>
-            <li class="list-inline-item mr-3">
-              <a href="#">
-                <i class="fab fa-twitter-square fa-2x fa-fw"></i>
-              </a>
-            </li>
-            <li class="list-inline-item">
-              <a href="#">
-                <i class="fab fa-instagram fa-2x fa-fw"></i>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </footer>
+             if($i["email"]==$email &&$i["password"]==$geslo ){
+               $pot = rtrim(dirname($_SERVER["PHP_SELF"]), "/\\");
+               $streznik = $_SERVER["HTTP_HOST"];
+               $preveri=true;
+           $_SESSION["id"] =$i["id"];
+           $_SESSION["ime"] =$i["ime"];
+           $_SESSION["priimek"] =$i["priimek"];;
+             header("Location: http://$streznik$pot/knjiznica.php");
 
+             }
+
+
+            }
+           }
+
+  if($preveri==false){
+   ?>
+
+<h3>Neuspešna prijava!</h3>
+<?php }} ?>
   <!-- Bootstrap core JavaScript -->
   <script src="vendor/jquery/jquery.min.js"></script>
   <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
