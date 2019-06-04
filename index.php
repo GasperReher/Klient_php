4039b8a64dcdb105b10d51c9fb3c59eea680f81c
GasperReher <gasper.reher@student.um.si>
2019-06-04 18:33:03 +0200

    Izpis izposoj

--- index.php
+++ index.php
@@ -37,51 +37,7 @@ $_SERVER['REQUEST_METHOD']
 
 
 
-<?php
-  if(array_key_exists('prijava',$_POST)){
-    $fields = array("method" => "mymethod", "email" => "myemail");
-    //echo $isci." ".$cat;ž
-    $email=$_POST['email'];
-    $geslo=$_POST['geslo'];
-    $url = "http://localhost:8880/projekt/rest/upoti/upot/";
-      $fields = json_encode($fields);
-      $ch = curl_init();
 
-      curl_setopt($ch, CURLOPT_URL, $url);
-      //curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-      curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-      curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-      'Content-Type: application/json',
-      'Content-Length: ' . strlen($fields))
-      );
-      $result= curl_exec($ch);
-
-      curl_close($ch);
-      $obj= json_decode($result,true);
-
-$preveri=false;
-            if(isset($obj)){
-            foreach($obj as $i) { //foreach element in $arr
-
-
-           if($i["email"]==$email &&$i["password"]==$geslo ){
-             $pot = rtrim(dirname($_SERVER["PHP_SELF"]), "/\\");
-             $streznik = $_SERVER["HTTP_HOST"];
-             $preveri=true;
-         $_SESSION["id"] =$i["id"];
-         $_SESSION["ime"] =$i["ime"];
-         $_SESSION["priimek"] =$i["priimek"];
-         $_SESSION["qr"] =$i["qrUporabnik"];
-           header("Location: http://$streznik$pot/knjiznica.php");
-
-           }
-
-
-          }
-         }
-}
- ?>
   <!-- Navigation -->
   <nav class="navbar navbar-light bg-light static-top">
     <div class="container">
@@ -122,6 +78,7 @@ $preveri=false;
       //echo $isci." ".$cat;ž
       $email=$_POST['email'];
       $geslo=$_POST['geslo'];
+
       $url = "http://localhost:8880/projekt/rest/upoti/upot/";
         $fields = json_encode($fields);
         $ch = curl_init();
@@ -134,16 +91,20 @@ $preveri=false;
         'Content-Type: application/json',
         'Content-Length: ' . strlen($fields))
         );
-        $result= curl_exec($ch);
-
+        $res= curl_exec($ch);
+    //  print_r($result);
         curl_close($ch);
-        $obj= json_decode($result,true);
-
+        $obja= json_decode($res,true);
+        print_r($obja);
   $preveri=false;
-              if(isset($obj)){
-              foreach($obj as $i) { //foreach element in $arr
+  echo $obja;
+              if(isset($obja)){
+              foreach($obja as $i) { //foreach element in $arr
 
-
+                echo $i["email"]." ".$i["password"];
+                $valid1=strcmp($i["email"],$email);
+                $valid2=strcmp($i["password"],$geslo);
+                echo $valid1." ".$valid2;
              if($i["email"]==$email &&$i["password"]==$geslo ){
                $pot = rtrim(dirname($_SERVER["PHP_SELF"]), "/\\");
                $streznik = $_SERVER["HTTP_HOST"];
@@ -151,6 +112,7 @@ $preveri=false;
            $_SESSION["id"] =$i["id"];
            $_SESSION["ime"] =$i["ime"];
            $_SESSION["priimek"] =$i["priimek"];;
+          $_SESSION["qr"] =$i["qrUporabnik"];;
              header("Location: http://$streznik$pot/knjiznica.php");
 
              }
