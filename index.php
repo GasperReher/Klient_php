1fe2182d49eac44911ff90fb06bbcc16fc5806bd
zigalepi <ziga.susin@gmail.com>
2019-06-04 20:30:24 +0200

    1

--- index.php
+++ index.php
@@ -38,48 +38,6 @@ $_SERVER['REQUEST_METHOD']
 
 
 
-<?php
-  if(array_key_exists('prijava',$_POST)){
-    $fields = array("method" => "mymethod", "email" => "myemail");
-    //echo $isci." ".$cat;ž
-    $email=$_POST['email'];
-    $geslo=$_POST['geslo'];
-    $url = "http://localhost:8880/projekt/rest/upoti/upot/";
-      $fields = json_encode($fields);
-      $ch = curl_init();
-
-      curl_setopt($ch, CURLOPT_URL, $url);
-      //curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-      curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-      curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-      'Content-Type: application/json',
-      'Content-Length: ' . strlen($fields))
-      );
-      $result= curl_exec($ch);
-
-      curl_close($ch);
-      $obj= json_decode($result,true);
-
-$preveri=false;
-            if(isset($obj)){
-            foreach($obj as $i) { //foreach element in $arr
-
-
-           if($i["email"]==$email &&$i["password"]==$geslo ){
-             $pot = rtrim(dirname($_SERVER["PHP_SELF"]), "/\\");
-             $streznik = $_SERVER["HTTP_HOST"];
-             $preveri=true;
-         $_SESSION["id"] =$i["id"];
-         $_SESSION["ime"] =$i["ime"];
-         $_SESSION["priimek"] =$i["priimek"];
-         $_SESSION["qr"] =$i["qrUporabnik"];
-
-           header("Location: http://$streznik$pot/knjiznica.php");
-
-           }
-
-
   <!-- Navigation -->
   <nav class="navbar navbar-light bg-light static-top">
     <div class="container">
@@ -120,11 +78,9 @@ $preveri=false;
       //echo $isci." ".$cat;ž
       $email=$_POST['email'];
       $geslo=$_POST['geslo'];
-
       $url = "http://localhost:8880/projekt/rest/upoti/upot/";
         $fields = json_encode($fields);
         $ch = curl_init();
-
         curl_setopt($ch, CURLOPT_URL, $url);
         //curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
@@ -142,7 +98,6 @@ $preveri=false;
   echo $obja;
               if(isset($obja)){
               foreach($obja as $i) { //foreach element in $arr
-
                 echo $i["email"]." ".$i["password"];
                 $valid1=strcmp($i["email"],$email);
                 $valid2=strcmp($i["password"],$geslo);
@@ -156,13 +111,9 @@ $preveri=false;
            $_SESSION["priimek"] =$i["priimek"];;
           $_SESSION["qr"] =$i["qrUporabnik"];;
              header("Location: http://$streznik$pot/knjiznica.php");
-
              }
-
-
             }
            }
-
   if($preveri==false){
    ?>
 
